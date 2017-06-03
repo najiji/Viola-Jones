0ca1ab6df1fd9f10770c3b1e25f1e05277b4dd4e
najiji <thomas.debrunner@hotmail.com>
2017-06-03 11:23:44 +0100

    added new trainer

--- src/de/fu/violajones/Train.py
+++ src/de/fu/violajones/Train.py
@@ -2,6 +2,7 @@ from de.fu.violajones.IntegralImage import IntegralImage
 from de.fu.violajones.HaarLikeFeature import FeatureTypes, HaarLikeFeature
 import numpy as np
 from joblib import Parallel, delayed, cpu_count
+from joblib.pool import has_shareable_memory
 import os
 import copy
 import pickle
@@ -29,6 +30,44 @@ def generate_features(size):
     return features
 
 
+
+# find / update threshold and coeff for each feature
+def feature_eval(feature, used):
+    if used:
+        return feature, np.inf
+
+    # find the scores for the images
+    n_img = len(images)
+    scores = np.zeros(n_img)
+    for i, img in enumerate(images):
+        scores[i] = feature.get_score(img)
+    sorted_img_args = np.argsort(scores)
+    Sp = np.zeros(n_img)  # sum weights for positive examples below current img
+    Sn = np.zeros(n_img)  # sum weights for negative examples below current img
+    Tp = 0
+    Tn = 0
+    for img_arg in np.nditer(sorted_img_args):
+        if labels[img_arg] == 0:
+            Tn += w[img_arg]
+        else:
+            Tp += w[img_arg]
+        Sn[img_arg] = Tn
+        Sp[img_arg] = Tp
+
+    # compute the formula for the threshold
+    nerror = Sp + (Tn - Sn)  # error of classifying everything negative below threshold
+    perror = Sn + (Tp - Sp)  # error of classifying everything positive below threshold
+    error = np.minimum(perror, nerror)  # find minimum
+    best_threshold_img = np.argmin(error)  # find the image with the threshold
+    best_local_error = error[best_threshold_img]
+    feature.threshold = scores[best_threshold_img]  # use the score we estimated for the image as new threshold
+    # assign new polarity, based on above calculations
+    feature.polarity = 1 if nerror[best_threshold_img] < perror[best_threshold_img] else -1
+
+    # store the error to find best feature
+    return feature, best_local_error
+
+
 def train(positive, negative, T):
     # count
     n_pos = len(positive)
@@ -38,6 +77,7 @@ def train(positive, negative, T):
     final_classifiers = []
 
     # gather images, extract labels
+    global images, labels, w
     images = np.hstack((np.array(positive), np.array(negative)))
     labels = [img.label for img in images]
 
@@ -50,8 +90,8 @@ def train(positive, negative, T):
     features = generate_features(20)
 
     # generate error list
-    n_features = len(features)
-    errors = np.zeros(n_features)
+#    n_features = len(features)
+#    errors = np.zeros(n_features)
 
     # ADABoost loop -------------------
     for round in range(T):
@@ -60,48 +100,16 @@ def train(positive, negative, T):
         # normalize the weights
         w = w/np.sum(w)
 
-        # find / update threshold and coeff for each feature
-        def feature_job(feature_nr, feature):
-        #    if (feature_nr+1) % 1000 == 0:
-        #        print('[ %d of %d ]'%(feature_nr+1, n_features))
-            if feature_nr in used:
-                return
-
-            # find the scores for the images
-            scores = np.zeros(n_img)
-            for i, img in enumerate(images):
-                scores[i] = feature.get_score(img)
-            sorted_img_args = np.argsort(scores)
-            Sp = np.zeros(n_img)  # sum weights for positive examples below current img
-            Sn = np.zeros(n_img)  # sum weights for negative examples below current img
-            Tp = 0
-            Tn = 0
-            for img_arg in np.nditer(sorted_img_args):
-                if labels[img_arg] == 0:
-                    Tn += w[img_arg]
-                    Sn[img_arg] = Tn
-                else:
-                    Tp += w[img_arg]
-                    Sp[img_arg] = Tp
-
-            # compute the formula for the threshold
-            nerror = Sp + (Tn - Sn)  # error of classifying everything negative below threshold
-            perror = Sn + (Tp - Sp)  # error of classifying everything positive below threshold
-            error = np.minimum(perror, nerror)  # find minimum
-            best_threshold_img = np.argmin(error)  # find the image with the threshold
-            best_local_error = error[best_threshold_img]
-            feature.threshold = scores[best_threshold_img]  # use the score we estimated for the image as new threshold
-            # assign new polarity, based on above calculations
-            feature.polarity = 1 if nerror[best_threshold_img] < perror[best_threshold_img] else -1
-
-            # store the error to find best feature
-            errors[feature_nr] = best_local_error
+        result = list(map(list, zip(*(Parallel(n_jobs=N_CPUS, max_nbytes=1e6, verbose=1)(delayed(feature_eval)
+                                                              (feature, feature_nr in used)
+                                                              for feature_nr, feature in enumerate(features))))))
+        features = result[0]
+        errors = np.array(result[1])
 
         # select best feature
         best_feature_arg = np.argmin(errors)
         best_feature = features[np.asscalar(best_feature_arg)]
         best_error = errors[best_feature_arg]
-        errors[best_feature_arg] = np.inf  # set error of just selected feature to infinity, to prevent re selecting it
         used.append(best_feature_arg)
 
         # compute beta
@@ -124,12 +132,17 @@ def train(positive, negative, T):
 
 
 def main():
+    RED = 1000
+
     print('Loading faces..')
     faces = load_images('train/face', 1)
-    print('..done. ' + str(len(faces)) + ' faces loaded.\n\nLoading non faces..')
+    print('..done. %d faces loaded, reducing to %d' % (len(faces), RED))
+    faces = faces[:RED]
+    print('Loading non-faces')
     non_faces = load_images('train/non-face', 0)
-    print('..done. ' + str(len(non_faces)) + ' non faces loaded.\n')
-    T = 3000
+    print('..done. %d non faces loaded, reducing to %d' % (len(non_faces), RED))
+    non_faces = non_faces[:RED]
+    T = 300
     train(faces, non_faces, T)
 
 if __name__ == '__main__':
